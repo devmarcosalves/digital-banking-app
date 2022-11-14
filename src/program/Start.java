9e043d62884b9ad64ea0dff193e995cf52b34132
Marcos Alves <102601006+devmarcosalves@users.noreply.github.com>
2022-11-14 15:04:20 -0300

    first commit
    Criação dos pacotes program e users;
    Criação da classe Start e seus atributos;
    Criação das classes Manager e FullManager;
    Importação do Scanner.

---
+++ src/program/Start.java
@@ -0,0 +1,24 @@
+package program;
+import java.util.Scanner;
+
+public class Start {
+
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		sc.close();
+	}
+
+	public static void login(){
+		Scanner sc = new Scanner(System.in);
+
+		sc.close();
+	}
+	public static void cadastro(){
+		Scanner sc = new Scanner(System.in);
+
+		sc.close();
+	}
+
+}
