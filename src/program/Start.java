1c06d5249a992e8dc4b157927ee984316d913209
Marcos Alves <102601006+devmarcosalves@users.noreply.github.com>
2022-11-14 17:00:45 -0300

    fourth commit
    Implementação do método de autorização de um novo usuário cadastrado para o FulManager autorizar;
    Implementação do método para depósito sem precisar ter cadastro ou sem fazer login, mediante autorização do gerente;
    Criação do pacote Screen e da classe HomeScreen.

--- src/program/Start.java
+++ src/program/Start.java
@@ -5,7 +5,7 @@ import users.Holders;
 //import users.Manager;
 
 public class Start {
-	private static int aux;
+		
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -14,7 +14,7 @@ public class Start {
 		int opc;
 		
 		do {
-			System.out.print("\n\n1. Login 2.Cadastrar 0.Sair");
+			System.out.print("\n\n1. Login 2.Cadastro \n3.Depósito 0.Sair");
 			System.out.print("\nEscolha uma opção: ");
 			opc = sc.nextInt();
 			switch(opc) {
@@ -22,7 +22,7 @@ public class Start {
 				login();
 				break;
 			case 2:
-				cadastro();
+				registration();
 				break;
 			case 0:
 				System.out.print("\nSaindo...");
@@ -47,10 +47,9 @@ public class Start {
 		}
 		//Precisa conferir no banco de dados se já possui cadastro
 		//para depois redirecionar ao menu
-		
-		sc.close();
+		//sc.close();
 	}
-	public static void cadastro(){
+	public static void registration(){
 		Scanner sc = new Scanner(System.in);
 		System.out.print("\nInforme seu Nome Completo: ");
 		Holders.name = sc.next();
@@ -75,14 +74,38 @@ public class Start {
 				System.out.print("\nVocê excedeu a quantidade máxima de tentativas");
 				System.out.print("\nPor favor, entre em contato com o Banco");
 			}
+			if(FullManager.authorization() == 1) {
+				Holders.saldo = 0;
+			}
 		}
-		FullManager ful = new FullManager();
-		aux = ful.authorization();
-		if(aux == 1) {
-			Holders.saldo = 0;
+		//sc.close();
+	}
+	public static void deposit() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\nInforme o CPF da conta de destino: ");
+		int cpfdestino = sc.nextInt();
+		System.out.print("Digite o valor que deseja depositar: ");
+		double valordep = sc.nextDouble();
+		//Pesquisa no Banco de Dados os dados da pessoa e exibe abaixo
+		if(cpfdestino == Holders.cpf) {
+			System.out.print("\nConfirme os dados");
+			System.out.println("\nNome: "+Holders.name);
+			System.out.println("CPF: "+Holders.cpf);
+			System.out.println("Valor: R$"+valordep);
+			System.out.print("\nTecle 1 para confirma ou 0 para cancelar: ");
+			int aux = sc.nextInt();
+			if(aux == 1) {
+				System.out.print("\nProcedimento finalizado com sucesso!");
+				System.out.print("\nO valor será liberado após a confirmação do Gerente.");
+				//Manager.confirmation();
+			}else
+				System.out.println("Procedimento Cancelado");
+		}else {
+			System.out.print("Conta não identificada");
 		}
 		
-		sc.close();
+		//cpfdestino = sc.nextInt();
+		//sc.close();
 	}
 
 }
--- src/users/FullManager.java
+++ src/users/FullManager.java
@@ -1,12 +1,21 @@
 package users;
 
+import java.util.Scanner;
+
 public class FullManager {
 
 	
-	public int authorization() {
-		int x=1;
+	public static int authorization() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\nNOVO CADASTRO DE USUÁRIO");
+		System.out.print("Nome: "+Holders.name);
+		System.out.print("CPF: "+Holders.cpf);
+		System.out.print("Digite 1 para AUTORIZAR ou 0 para NEGAR: ");
+		int x = sc.nextInt();
 		
 		
+		sc.close();
+
 		return x;
 	}
 	
